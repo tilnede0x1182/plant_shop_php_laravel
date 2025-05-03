33f57519ce849a343c792f6a87d89de65e1e67aa
Cyrille DEGASQUET <pro.cyrille.degasquet@gmail.com>
2025-05-03 21:22:12 +0200

    Correction de l'affichage des boutons dans plant show

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/css/orange.css', // 👈 ajoute ceci
+                'resources/css/orange.css',
                 'resources/js/app.js',
             ],
             refresh: true,
